e4a08578e4042d060f29e56210802fed007ab77e
Bartek <mbanzerberg@gmail.com>
2023-09-07 10:36:18 +0200

    First version of Home Page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,109 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="banner">
+      <div class="navbar">
+        <div class="dumbbell-icon"></div>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+        </ul>
+      </div>
+
+      <div class="content">
+        <h1>Gym Assistant</h1>
+        <div class="buttons">
+          <button-item text="Dodaj ćwiczenie"/>
+          <button-item text="Dodaj nowe dane"/>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ButtonItem from "@/components/ButtonItem";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {ButtonItem}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
 }
+
+.dumbbell-icon {
+  width: 40px;
+  height: 40px;
+  background-image: linear-gradient(rgba(0,0,0,0.75),rgba(0,0,0,0.75)),url("assets/dumbbell_icon.png");
+  background-size: 40px;
+  background-position: left;
+}
+
+.banner {
+  width: 100%;
+  height: 100vh;
+  background-image: linear-gradient(rgba(0,0,0,0.75),rgba(0,0,0,0.75)),url("assets/gym.jpg");
+  background-size: cover;
+  background-position: center;
+}
+
+.navbar {
+  width: 85%;
+  margin: auto;
+  padding: 35px 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.navbar ul li {
+  list-style: none;
+  display: inline-block;
+  margin: 0 20px;
+  position: relative;
+}
+
+.navbar ul li a {
+  text-decoration: none;
+  color: #fff;
+  text-transform: uppercase;
+}
+
+.navbar ul li::after {
+  content: '';
+  height: 3px;
+  width: 0px;
+  background: #009688;
+  position: absolute;
+  left: 0;
+  bottom: -10px;
+  transition: 0.5s;
+}
+
+.navbar ul li:hover::after {
+  width: 100%;
+}
+
+.content {
+  width: 100%;
+  position: relative;
+  top: 35%;
+  transform: translateY(-50%);
+  text-align: center;
+  color: #fff;
+}
+
+.content .buttons {
+  margin: 40px 20px;
+}
+
+.content h1 {
+  font-size: 70px;
+  margin-top: 80px;
+}
+
 </style>
---
+++ frontend/src/components/ButtonItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="content">
+    <button><span></span>{{text}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonItem",
+  props: ['text']
+}
+</script>
+
+<style scoped>
+
+.content button {
+  width: 200px;
+  padding: 15px 0;
+  text-align: center;
+  margin: 10px 10px;
+  border-radius: 25px;
+  font-weight: bold;
+  border: 2px solid #009688;
+  background: transparent;
+  color: #fff;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+
+.content span {
+  background: #009688;
+  height: 100%;
+  width: 0;
+  border-radius: 25px;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  z-index: -1;
+  transition: 0.5s;
+}
+
+.content button:hover span {
+  width: 100%;
+}
+
+.content button:hover {
+  border: none;
+}
+
+</style>
